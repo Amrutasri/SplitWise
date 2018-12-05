fd5833833f8ad9aff3ae92b74f85e697d61c3020
sindhu <amruta.talam@gmail.com>
2018-12-05 15:26:02 +0530

    Sindhu | Removed unnecessary print() statements.

--- src/main/java/SplitWise.java
+++ src/main/java/SplitWise.java
@@ -29,30 +29,24 @@ public class SplitWise {
             Expense expense = friend.getExpense();
             if ((expense.isGreater(expenseOnEach))) {
                 expense.setAmountToGetBack(expense.subtract(expenseOnEach));
-                System.out.println("amount to get back " + expense.getAmountToGetBack());
             } else {
                 expense.setAmountToRepay(expense.subtract(expenseOnEach));
-                System.out.println("amount to repay " + expense.getAmountToRepay());
             }
         }
     }
 
     private void resolve(Friend creditor, double creditorAmountToGetBack) {
-        System.out.println("CreditorAmountToGetBack = " + creditorAmountToGetBack);
         for (Friend debtor : friends) {
             Expense debtorExpense = debtor.getExpense();
             double debtorExpenseToRepay = debtorExpense.getAmountToRepay();
             if (debtorExpenseToRepay != 0) {
                 if (debtorExpenseToRepay == creditorAmountToGetBack) {
-                    System.out.println("yes1");
                     creditors.add(creditor); debtors.add(debtor);
                     break;
                 } else if (!(debtorExpenseToRepay > creditorAmountToGetBack)) {
-                    System.out.println("yes3");
                     creditorAmountToGetBack = creditorAmountToGetBack - debtorExpenseToRepay;
                     creditors.add(creditor); debtors.add(debtor);
                 } else if (debtorExpenseToRepay > creditorAmountToGetBack) {
-                    System.out.println("yes2");
                     creditors.add(creditor); debtors.add(debtor);
                     debtorExpense.setAmountToRepay(debtorExpense.subtract(creditorAmountToGetBack));
                     break;
