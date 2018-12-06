7de70ed585c9f76317394ec7ea7938c7b49943ff
sindhu <amruta.talam@gmail.com>
2018-12-06 12:22:05 +0530

    Sindhu | Included test case for taking input as double method for InputDriver class.

---
+++ src/test/java/InputDriverTest.java
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InputDriverTest {
+
+    @DisplayName("should return 15.47 which is double.")
+    @Test
+    void shouldReturn15Point47WhichIsDouble() {
+        setSystemIn("15.47");
+
+        InputDriver inputDriver = new InputDriver();
+
+        assertEquals(15.47,inputDriver.readInputAsDouble());
+    }
+
+    private void setSystemIn(String string) {
+        System.setIn(new ByteArrayInputStream(string.getBytes()));
+    }
+
+    @AfterEach
+    void setUp() {
+        System.setIn(System.in);
+    }
+
+}
