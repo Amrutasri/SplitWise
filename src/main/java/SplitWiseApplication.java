2a95abc38977d38c8a2966bfc8f5f18e62a72d9e
sindhu <amruta.talam@gmail.com>
2018-12-12 17:18:45 +0530

    Sindhu | Modified SplitWiseApplication which is an UI based on the refactoring done by introducing Transactions into SplitWise class.

--- src/main/java/SplitWiseApplication.java
+++ src/main/java/SplitWiseApplication.java
@@ -1,38 +1,81 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class SplitWiseApplication {
 
+    private static InputDriver inputDriver = new InputDriver();
+    private static OutputDriver outputDriver = new OutputDriver();
+
     public static void main(String args[]) {
 
-        InputDriver inputDriver = new InputDriver();
-        OutputDriver outputDriver = new OutputDriver();
+        outputDriver.print("Enter number of friends ");
+        int numberOfFriends = getValidInteger();
 
-        outputDriver.print("Enter number of friends in trip: ");
-        int numberOfFriends = inputDriver.readInputAsInteger();
+        boolean isValid = isValid(numberOfFriends);
 
-        List<Friend> friends = new ArrayList<>(numberOfFriends);
+        if(isValid) {
+            List<Friend> friends = new ArrayList<>(numberOfFriends);
 
-        for(int index = 0 ; index<numberOfFriends ; index++) {
-            outputDriver.print("Enter name of friend: ");
-            String name = inputDriver.readInputAsString();
-            outputDriver.print("Enter amount spent: ");
-            double amountSpent = inputDriver.readInputAsDouble();
-            Money money = new Money(amountSpent);
-            Friend friend = new Friend(name,money);
-            friends.add(friend);
+            for (int index = 0; index < numberOfFriends; index++) {
+                outputDriver.print("Enter name of friend: ");
+                String name = getValidString();
+                outputDriver.print("Enter amount spent: ");
+                int amountSpent = getValidInteger();
+                Friend friend = new Friend(name, amountSpent);
+                friends.add(friend);
+            }
+
+            Trip trip = new Trip(friends);
+
+            SplitWise splitWise = new SplitWise(trip);
+            List<Transaction> transactions = splitWise.settle();
+
+            for (Transaction transaction : transactions) {
+                outputDriver.print(transaction.getDebtor().getName());
+                outputDriver.print("->");
+                outputDriver.print(transaction.getCreditor().getName());
+                outputDriver.print(", ");
+                outputDriver.print(transaction.getAmount());
+                outputDriver.print("\n");
+            }
         }
-
-        Trip trip = new Trip(friends);
-
-        SplitWise splitWise = new SplitWise(trip,outputDriver);
-        splitWise.settle();
-        for(int i=0; i<debtors.size(); i++) {
-            outputDriver.print(debtors.get(i).getName());
-            outputDriver.print(" -> ");
-            outputDriver.print(creditors.get(i).getName());
-            outputDriver.print(" = ");
-            outputDriver.print(debtors.get(i).getMoney().getAmountToRepay());
+        else {
+            outputDriver.print("SplitWise needs minimum of 2 friends. ");
         }
+    }
+
+    private static int getValidInteger() {
+        boolean isInt = true;
+        String input = "";
+        int numberOfFriends;
+        Pattern pattern = Pattern.compile( ".*[^0-9].*" );
+        while (isInt) {
+            input = inputDriver.readInputAsString();
+            isInt = pattern.matcher(input).matches();
+            if (isInt) {
+                outputDriver.print("Enter a valid number. ");
+            }
+        }
+        numberOfFriends = Integer.parseInt(input);
+        return numberOfFriends;
+    }
+
+    private static String getValidString() {
+        boolean isString = false;
+        String input1 = "";
+        Pattern pattern1 = Pattern.compile(".*[^a-z][^A-Z].*");
+        while (!(isString)) {
+            input1 = inputDriver.readInputAsString();
+            isString = pattern1.matcher(input1).matches();
+            if(!(isString)) {
+                outputDriver.print("Enter a valid name. ");
+            }
+        }
+        return input1;
+    }
+
+    private static boolean isValid(int numberOfFriends) {
+        return numberOfFriends >= 2;
     }
 }
