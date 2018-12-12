3cc1d52cfe2af2032ce6740b0729e71db5a86eb9
sindhu <amruta.talam@gmail.com>
2018-12-12 14:16:30 +0530

    Sindhu | added new test case for Transaction class and implemented it by overriding equals and hash() methods.

--- src/main/java/Transaction.java
+++ src/main/java/Transaction.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Transaction {
 
     private double amount;
@@ -20,5 +22,20 @@ public class Transaction {
 
     Friend getDebtor() {
         return debtor;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        Transaction that = (Transaction) object;
+        return Double.compare(that.amount, amount) == 0 &&
+                Objects.equals(creditor, that.creditor) &&
+                Objects.equals(debtor, that.debtor);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(amount, creditor, debtor);
     }
 }
--- src/test/java/TransactionTest.java
+++ src/test/java/TransactionTest.java
@@ -1,26 +1,44 @@
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 class TransactionTest {
 
     Friend friendA = new Friend("A",100);
     Friend friendB = new Friend("B",40);
-    Transaction transaction = new Transaction(30,friendA,friendB);
+    Transaction transaction1 = new Transaction(30,friendA,friendB);
+    Transaction transaction2 = new Transaction(40,friendA,friendB);
+    Transaction transaction3 = new Transaction(30,friendA,friendB);
 
     @Test
     void shouldReturn40AsAmount() {
-        assertEquals(30,transaction.getAmount());
+        assertEquals(30, transaction1.getAmount());
     }
 
     @Test
     void shouldReturnFriendAAsCreditor() {
-        assertEquals(friendA,transaction.getCreditor());
+        assertEquals(friendA, transaction1.getCreditor());
     }
 
     @Test
     void shouldReturnFriendBAsDebtor() {
-        assertEquals(friendB,transaction.getDebtor());
+        assertEquals(friendB, transaction1.getDebtor());
+    }
+
+    @Test
+    void shouldEqualTransactionItself() {
+        assertEquals(transaction1, transaction1);
+    }
+
+    @Test
+    void shouldEqualTransaction3WhichIsSameAsTransaction() {
+        assertEquals(transaction1,transaction3);
+    }
+
+    @Test
+    void shouldNotEqualTransactionWhereTheAmountIsDifferent() {
+        assertNotEquals(transaction1,transaction2);
     }
 
 }
